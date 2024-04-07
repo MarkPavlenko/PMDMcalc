9a0ebcf7d7d45f8aa5eac476701dffbef5389372
markdev <markpavlenkos@gmail.com>
2024-04-07 19:15:42 +0300

    removing errors

--- calc.py
+++ calc.py
@@ -6,20 +6,47 @@ if operation == "+":
     result = num1 + num2
     print(result)
 
+if operation == " +":
+    num1 = float(input("Number 1:"))
+    num2 = float(input("Number 2:"))
+    result = num1 + num2
+    print(result)
+
 elif operation == "-":
     num1 = float(input("Number 1:"))
     num2 = float(input("Number 2:"))
     result = num1 - num2
     print(result)
+
+elif operation == " -":
+    num1 = float(input("Number 1:"))
+    num2 = float(input("Number 2:"))
+    result = num1 - num2
+    print(result)
+
 elif operation == "/":
     num1 = float(input("Number 1:"))
     num2 = float(input("Number 2:"))
     result = num1 / num2
     print(result)
+
+elif operation == " /":
+    num1 = float(input("Number 1:"))
+    num2 = float(input("Number 2:"))
+    result = num1 / num2
+    print(result)
+
 elif operation == "*":
     num1 = float(input("Number 1:"))
     num2 = float(input("Number 2:"))
     result = num1 * num2
     print(result)
+
+elif operation == " *":
+    num1 = float(input("Number 1:"))
+    num2 = float(input("Number 2:"))
+    result = num1 * num2
+    print(result)
+
 else:
     print("Your symbol is not supported in PMDMcalc. Please try again with another symbol.")
